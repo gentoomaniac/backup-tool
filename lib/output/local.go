dedec36d20f5dc09723c799d2e2212b0f6888008
Marco Siebecke <marco@siebecke.se>
2019-11-27 23:59:50 +0100

    basic idea for output and imports

--- lib/output/local.go
+++ lib/output/local.go
@@ -1,10 +1,10 @@
 package local
 
 import (
-	"github.com/gentoomaniac/backup-tool/model"
+	"github.com/gentoomaniac/backup-tool/src/model"
 	log "github.com/sirupsen/logrus"
 )
 
-func Write(block model.*Block) error {
+func Write(block *model.Block) error {
 	log.Debug("test")
 }
